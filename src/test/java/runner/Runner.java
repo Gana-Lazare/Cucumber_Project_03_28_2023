df1e0e3b30d3d96bd19597b2a137fc40543242ce
Gana-Lazare <autolazare@gmail.com>
2023-05-18 14:17:25 -0400

    add read file for curl

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,7 +9,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
         features = {"src/test/java/features"}, //the path of the feature files
-        glue = "stepDefinitions", //the path of the step definition files
+        glue = "stepDefinitions",
+        tags = "@regression",
+//        tags = "@tag1 or @tag2"  // Executes scenarios with either @tag1 or @tag2
+//        tags = "@tag1 and not @tag2"  // Executes scenarios with @tag1 but not @tag2//the path of the step definition files
         plugin = {"pretty",
                 "json:target/cucumber-reports/cucumber.json",
                 "html:target/cucumber-reports/index.html",
@@ -19,5 +22,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
         monochrome = true, //display the console output in a proper readable format
         dryRun = false, //to check the mapping is proper between feature file and step def file
         publish = true)
+// Run the command mvn test -Dcucumber.options="--tags @tag1" to execute from local specific tags
 public class Runner extends AbstractTestNGCucumberTests {
 }
